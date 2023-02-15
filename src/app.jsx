2bd2974d2fd2a7d7aad2e559d53718803ca73b7d
arm32x <arm32x@arm32.ax>
2023-02-15 09:42:04 -0500

    Make mostly everything look decent

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,14 @@
-import { json } from "@codemirror/lang-json";
+import { json, jsonParseLinter } from "@codemirror/lang-json";
 import { mountDom } from "@gera2ld/jsx-dom";
+import { keymap } from "@codemirror/view";
 import { basicSetup, EditorView } from "codemirror";
-import { materialDark } from "cm6-theme-material-dark";
+import { materialDarkHighlightStyle } from "cm6-theme-material-dark";
 
 import { ChevronRight } from "./icons.jsx";
 import stylesheet from "./style.css";
+import { indentWithTab } from "@codemirror/commands";
+import { syntaxHighlighting } from "@codemirror/language";
+import { linter, lintGutter } from "@codemirror/lint";
 
 GM_addStyle(stylesheet);
 
@@ -30,17 +34,115 @@ const observer = new MutationObserver((_, observer) => {
 observer.observe(mountpoint, { childList: true, subtree: true });
 
 const JsonEditor = () => {
-  const extensions = [basicSetup, json(), materialDark];
+  const theme = EditorView.theme(
+    {
+      "&": {
+        fontSize: "14px",
+        lineHeight: "1.4em",
+      },
+      "&, .cm-panels": {
+        color: "var(--ReCK-foreground-color)",
+        backgroundColor: "var(--ReCK-background-color)",
+      },
+      ".cm-panels.cm-panels-top": {
+        borderBottom: "1px solid var(--ReCK-divider-color)",
+      },
+      ".cm-panels.cm-panels-bottom": {
+        borderTop: "1px solid var(--ReCK-divider-color)",
+      },
+
+      ".cm-gutters": {
+        backgroundColor: "var(--ReCK-background-color)",
+        color: "var(--ReCK-secondary-color)",
+      },
+
+      ".cm-activeLine": {
+        backgroundColor: "var(--ReCK-editor-active-line-background-color)",
+      },
+      ".cm-activeLineGutter": {
+        backgroundColor: "var(--ReCK-editor-active-line-background-color)",
+      },
+
+      "&.cm-focused": {
+        outline: "1px solid var(--ReCK-focus-color-opaque)",
+      },
+      "&.cm-focused .cm-selectionBackground, .cm-selectionBackground, .cm-content ::selection":
+        {
+          backgroundColor: "var(--ReCK-selection-color)",
+        },
+      ".cm-selectionMatch": {
+        backgroundColor: "var(--ReCK-editor-selection-match-color)",
+      },
+
+      ".cm-content": {
+        caretColor: "var(--ReCK-foreground-color)",
+      },
+      ".cm-cursor, .cm-dropCursor": {
+        borderLeftColor: "var(--ReCK-foreground-color)",
+      },
+
+      ".cm-foldPlaceholder": {
+        backgroundColor: "transparent",
+        border: "none",
+        color: "var(--ReCK-secondary-color)",
+      },
+
+      ".cm-tooltip": {
+        border: "none",
+        backgroundColor: "var(--ReCK-background-color)",
+        borderRadius: "2px",
+        boxShadow: "var(--ReCK-editor-tooltip-shadow)",
+        overflow: "hidden",
+      },
+      ".cm-tooltip .cm-tooltip-arrow::before, .cm-tooltip .cm-tooltip-arrow::after":
+        {
+          borderTopColor: "transparent",
+          borderBottomColor: "transparent",
+        },
+
+      // Colors from the Material Dark theme (used for syntax highlighting).
+      ".cm-diagnostic-error": {
+        borderLeftColor: "#ff5f52",
+      },
+      ".cm-diagnostic-warning": {
+        borderLeftColor: "#ffad42",
+      },
+      ".cm-lintPoint::after": {
+        bottom: "7px",
+      },
+      ".cm-lintPoint-error::after": {
+        borderBottomColor: "#ff5f52",
+      },
+      ".cm-lintPoint-warning::after": {
+        borderBottomColor: "#ffad42",
+      },
+
+      // Work around weird line height on empty document.
+      ".cm-line": {
+        minHeight: "1.4em",
+      },
+    },
+    { dark: true }
+  );
+
   const codemirror = new EditorView({
-    extensions,
+    extensions: [
+      basicSetup,
+      keymap.of(indentWithTab),
+      json(),
+      linter(jsonParseLinter()),
+      theme,
+      syntaxHighlighting(materialDarkHighlightStyle),
+      EditorView.darkTheme.of(true),
+    ],
   });
 
   return (
     <>
       <div className="ReCK-header">
-        <h2 className="ReCK-header-title">
-          <span className="ReCK-logo">ReCK</span> JSON Editor
-        </h2>
+        <span className="ReCK-header-title">
+          <span className="ReCK-logo">ReCK </span>JSON Editor
+        </span>
       </div>
       <div className="ReCK-json-codemirror">{codemirror.dom}</div>
       <div className="ReCK-json-button-panel">
@@ -61,6 +163,11 @@ const sidebar = mountDom(
     </div>
   </div>
 );
+
+const eventsToStopPropagationFor = ["keydown", "keyup"];
+for (const event of eventsToStopPropagationFor) {
+  sidebar.addEventListener(event, (e) => e.stopPropagation());
+}
 
 const sidebarHandle = sidebar.getElementsByClassName("ReCK-sidebar-handle")[0];
 sidebarHandle.addEventListener("click", (_) => {
